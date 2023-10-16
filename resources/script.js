ede05613fe39cf28098a7b495508d08d5360d42a
higinofe <fernandohigino.fsh@gmail.com>
2023-10-16 12:01:02 -0300

    3commit

--- resources/script.js
+++ resources/script.js
@@ -1,6 +1,13 @@
 const btnRigth = window.document.querySelector('.button-arrow.-right');
 const btnLeft = window.document.querySelector('.button-arrow.-left');
 const elements = window.document.querySelector('.elements');
+const about = window.document.querySelector('#abouts');
+
+const openModalButton = document.querySelector('#open-modal');
+const closeModalButton = document.querySelector('#close-modal');
+const modal = document.querySelector('#modal');
+const fade = document.querySelector('#fade');
+
 let pixels = 50;
 
 btnRigth.addEventListener('click', function(){
@@ -10,4 +17,14 @@ btnRigth.addEventListener('click', function(){
 btnLeft.addEventListener('click', function(){
     pixels = pixels-50;
     elements.style = 'transform: translateX('+pixels+'px)';
+});
+
+
+const toglemodal = () => {
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+}
+
+[openModalButton, closeModalButton, fade].forEach((el) => { 
+    el.addEventListener("click", () => toglemodal());
 });
